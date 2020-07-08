7665bebe5aaeed48cde78a60f8b6329821b7294e
Vlados <vlados.pochta@gmail.com>
2020-07-09 01:03:16 +0400

    Add contact deut

---
+++ src/inline-menu/deut/ContactOrHelp.js
@@ -0,0 +1,16 @@
+import Telegraf from 'telegraf';
+import config from '../../../config';
+const { Markup } = Telegraf;
+
+const text = `✉️ Kontakt / Hilfe\n\nDu benötigst Hilfe bei der Anmeldung, hast Fragen zum Trading, möchtest einen lukrativen Trade oder deinen Tradingerfolg (Screenshot) mit der Gruppe teilen?\n\n➡️ Dann kontaktiere gerne Florian und sein Team hier auf Telegram:`;
+const username = config.CONTACT_USERNAME;
+
+const contact = (ctx) =>  {
+  ctx.reply(text, Markup.inlineKeyboard([
+    [Markup.urlButton("📩 Kontaktiere uns", `https://t.me/${username}`)],
+    [Markup.callbackButton("↩️ Zurück zur Anmeldung", "English_lang")],
+  ])
+  .extra());
+};
+
+export default contact;
