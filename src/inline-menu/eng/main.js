fb2326b71dd2ae10d8952f844dd6eee31cd96710
Vlados <vlados.pochta@gmail.com>
2020-07-08 23:54:37 +0400

    Upd url

--- src/inline-menu/eng/main.js
+++ src/inline-menu/eng/main.js
@@ -2,9 +2,11 @@ import Telegraf from 'telegraf';
 import config from '../../../config';
 const { Markup } = Telegraf;
 
+const URL = process.env.REGISTRATION_URL || config.REGISTRATION_URL;
+
 const text = `📈 Registration - Trading Group 📉\n\n` + 
 `With the following simple steps, you can become part of our successful trading community:\n\n` +
-`1. Sign up with your custom link at our verified and EU regulated broker: \nhttps://tcd.me/cfdstrade?payload=c4faz 📝\n\n` +
+`1. Sign up with your custom link at our verified and EU regulated broker: \n${URL}} 📝\n\n` +
 `❗️Please note: The registration works only with this link. Do you already have an account with this broker? Feel free to contact us! (see below "Contact"👇)\n\n` + 
 `2. Deposit any amount starting from 100€ to your new broker account. In order for you to start perfectly, we recommend a starting amount of 500€. Your deposit is safe with the verified and regulated broker and can be withdrawn free of charge. ✅\n\n` + 
 `3. Wait for your activation ⏱ (within 1h after registration of your new broker account). In the exceptional case that it takes longer, you are welcome to contact us and we will take care of it (see below). 🤝\n\n` + 
@@ -12,7 +14,6 @@ const text = `📈 Registration - Trading Group 📉\n\n` +
 `If you have any questions, Florian and his team are always at your disposal. 👥 👍` +
 `Please note and read the mandatory risk warning for financial products at:\n\n` +
 `/disclaimer`;
-const URL = process.env.REGISTRATION_URL || config.REGISTRATION_URL;
 
 const main = (bot, ctx) => {
   ctx.reply(text, Markup.inlineKeyboard([
